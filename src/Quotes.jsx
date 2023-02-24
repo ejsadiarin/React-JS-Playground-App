35799c4474087f4be51fd9ad1fbf5532716406bd
ejsadiarin <ejsadiarin@gmail.com>
2023-02-25 00:01:43 +0800

    TODO: display data from the API

--- src/Quotes.jsx
+++ src/Quotes.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+
 export default function Quotes() {
+  const [excuse, setExcuse] = useState("");
+  
   fetch('https://excuser-three.vercel.app/v1/excuse/')
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+      setExcuse(data.excuse);
     })
 
     return (
       <>
+        <p>{excuse}</p>
         <button>Get Quote</button>
       </>
     )
