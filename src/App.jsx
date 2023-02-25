725f9c876dfdff7776f0e7acd91a61bb49967651
ejsadiarin <ejsadiarin@gmail.com>
2023-02-25 14:57:18 +0800

    Added ObejctAPI Component, fetching and rendering data with null as default state and using null allow to render

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Sidebar from './Sidebar'
 import Exercise1 from './Exercise1'
 import TodoList from './TodoList'
 import Quotes from './Quotes'
+import ObjectAPI from './ObjectAPI'
 
 // TODO: make a UI that displays all the mini projects done so far
 // --> can be a Sidebar that has buttons to show Components (Planets, TodoList, etc.)
@@ -19,6 +20,7 @@ function App() {
       {/* <Exercise1 /> */}
       {/* <TodoList /> */}
       <Quotes />
+      <ObjectAPI />
     </div>
   )
 }
--- src/ObjectAPI.jsx
+++ src/ObjectAPI.jsx
@@ -1,13 +1,38 @@
 import { useState } from "react";
 
 export default function ObjectAPI() {
+  const [name, setName] = useState("");
   const [predictedAge, setPredictedAge] = useState(null);
+
+  function fetchPredictedAge() {
+    fetch(`https://api.agify.io/?name=${name}`)
+      .then(res => res.json())
+      .then(data => {
+        setPredictedAge(data);
+      })
+
+  }
 
   return (
     <>
-      <h3>Name:</h3>
-      <h3>Predicted Age:</h3>
-      <h3>Count:</h3>
+      <input
+      type="text" 
+      placeholder="Ex. Ej..."
+      onChange={(e) => {
+        setName(e.target.value);
+      }}
+      />
+      <button onClick={fetchPredictedAge}>Fetch Data</button>
+      <h3>Name: {predictedAge?.name}</h3>
+      <h3>Predicted Age: {predictedAge?.age}</h3>
+      <h3>Count: {predictedAge?.count}</h3>
     </>
   );
 }
+/**
+ * API Object response: can use useState(null) or useState({}) as default initial value
+ * --> then access properties of that object by:
+ * --> first in fetch: .then(data => setStateVar(data))
+ * --> inside return render: stateVar?.{key}
+ * --> the ? means null is allowed (since null is default initial value, need to activate first)
+ */
